724fd85455455c2df165d94fb0a3d6e431f9b605
= <fsilveira>
2019-05-03 11:29:01 -0300

    Finalizados os testes

--- src/br/facet/teste/TesteCliente.java
+++ src/br/facet/teste/TesteCliente.java
@@ -1,5 +1,8 @@
 package br.facet.teste;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 import br.facet.atm.Banco;
 import br.facet.atm.CaixaEletronico;
 
@@ -14,20 +17,88 @@ public class TesteCliente {
 	 */
 	Banco banco = new Banco();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		// TODO I. teste, diretamente no banco do cliente, a criação de uma nova conta para o cliente de id 9, com R$2000 na conta
-		// TODO II. teste, diretamente no banco do cliente, o encerramento da conta do cliente 2
-		// TODO III. teste, pelo caixa eletronico, o saque de R$389 do cliente 10
-		// TODO VI. teste, pelo caixa eletrono, o saque de R$1058 do cliente 3
-		// TODO V. teste, pelo caixa eletronico, o saque de R$10.000 do cliente 2
-		// TODO VI. teste, pelo caixa eletronico, o saque de R$12 do cliente 8
-		// TODO VII. teste, pelo caixa eletronico, o deposito de R$20 para o cliente 3
-		// TODO VIII. teste, pelo caixa eletronico, o saldo do cliente 2
-		// TODO XI. teste, pelo caixa eletronico, o saldo do cliente 3
-		// TODO X. teste
-		// TODO XI. teste
-		// TODO XII. mostre, pelo caixa eletronico, quanto de dinheiro ele ainda tem.
-		// TODO XIII. mostre, pelo caixa eletronico, quantas notas sobraram no caixa eletronico apos este dia de movimentação
+		TesteCliente teste = new TesteCliente();
+		
+		try {
+			// I. teste, diretamente no banco do cliente, a criação de uma nova conta para o cliente de id 9, com R$2000 na conta
+			boolean resultado = teste.banco.abreConta(9, 2000);
+			System.out.println("A abertura da conta funcionou? " + resultado);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
+		try {
+			// III. teste, pelo caixa eletronico, o saque de R$389 do cliente 10
+			Map<Integer, Integer> resultado = teste.caixaEletronico.fazerSaque(10, 389);
+			System.out.println(String.format("O cliente sacou as notas %s.", resultado));
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// VI. teste, pelo caixa eletrono, o saque de R$1058 do cliente 3
+			Map<Integer, Integer> resultado = teste.caixaEletronico.fazerSaque(3, 1058);
+			System.out.println(String.format("O cliente sacou as notas %s.", resultado));
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// V. teste, pelo caixa eletronico, o saque de R$10.000 do cliente 2
+			Map<Integer, Integer> resultado = teste.caixaEletronico.fazerSaque(2, 10000);
+			System.out.println(String.format("O cliente sacou as notas %s.", resultado));
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// II. teste, diretamente no banco do cliente, o encerramento da conta do cliente 2
+			boolean resultado = teste.banco.encerrarConta(2);
+			System.out.println("O encerramento da conta funcionou? " + resultado);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// VIII. teste, pelo caixa eletronico, o saldo do cliente 2
+			teste.caixaEletronico.mostrarSaldo(2);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// VI. teste, pelo caixa eletronico, o saque de R$12 do cliente 8
+			Map<Integer, Integer> resultado = teste.caixaEletronico.fazerSaque(8, 12);
+			System.out.println(String.format("O cliente sacou as notas %s.", resultado));
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
+		try {
+			// VII. teste, pelo caixa eletronico, o deposito de R$20 para o cliente 3
+			Map<Integer, Integer> resultado = teste.caixaEletronico.fazerSaque(3, 20);
+			System.out.println(String.format("O cliente sacou as notas %s.", resultado));
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// XI. teste, pelo caixa eletronico, o saldo do cliente 3
+			teste.caixaEletronico.mostrarSaldo(3);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		try {
+			// XII. mostre, pelo caixa eletronico, quanto de dinheiro ele ainda tem.
+			// XIII. mostre, pelo caixa eletronico, quantas notas sobraram no caixa eletronico apos este dia de movimentação
+			teste.caixaEletronico.mostrarSaldoCaixa();
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
